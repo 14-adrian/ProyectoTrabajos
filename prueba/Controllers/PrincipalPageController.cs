8134a3e2e3dea253d7264f247d90da783ad3dcef
Leoni <Leoni@WARMACHINE>
2023-06-02 19:22:42 -0600

    Pagina Finalizada

--- prueba/Controllers/PrincipalPageController.cs
+++ prueba/Controllers/PrincipalPageController.cs
@@ -12,150 +12,31 @@ namespace prueba.Controllers
             _context = context;
         }
 
+
         // GET: PrincipalPage
         public IActionResult Index()
         {
-            var empresa = (from m in _context.empresa
-                           select m).ToList();
 
+            var count = (from x in _context.empresa select x.empresaID).Count();
 
+            var count2 = (from x in _context.ofertas select x.ofertaID).Count();
+
+            var empresa = (from m in _context.ofertas
+                           select m).ToList()
+                           .OrderByDescending(m => m.ofertaID).Take(3);
+
+            var testimonios = (from n in _context.testimonios
+                               select n).ToList();
+
+            ViewData["contar"] = count;
+            ViewData["contarPlazas"] = count2;
+            ViewData["testimonios"] = testimonios;
             ViewData["empresa"] = empresa;
             return View();
 
 
         }
 
-        // GET: PrincipalPage/Details/5
-        //        public async Task<IActionResult> Details(int? id)
-        //        {
-        //            if (id == null || _context.empresa == null)
-        //            {
-        //                return NotFound();
-        //            }
 
-        //            var empresa = await _context.empresa
-        //                .FirstOrDefaultAsync(m => m.empresaID == id);
-        //            if (empresa == null)
-        //            {
-        //                return NotFound();
-        //            }
-
-        //            return View(empresa);
-        //        }
-
-        //        // GET: PrincipalPage/Create
-        //        public IActionResult Create()
-        //        {
-        //            return View();
-        //        }
-
-        //        // POST: PrincipalPage/Create
-        //        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        //        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //        [HttpPost]
-        //        [ValidateAntiForgeryToken]
-        //        public async Task<IActionResult> Create([Bind("empresaID,nombre,correo,telefono")] empresa empresa)
-        //        {
-        //            if (ModelState.IsValid)
-        //            {
-        //                _context.Add(empresa);
-        //                await _context.SaveChangesAsync();
-        //                return RedirectToAction(nameof(Index));
-        //            }
-        //            return View(empresa);
-        //        }
-
-        //        // GET: PrincipalPage/Edit/5
-        //        public async Task<IActionResult> Edit(int? id)
-        //        {
-        //            if (id == null || _context.empresa == null)
-        //            {
-        //                return NotFound();
-        //            }
-
-        //            var empresa = await _context.empresa.FindAsync(id);
-        //            if (empresa == null)
-        //            {
-        //                return NotFound();
-        //            }
-        //            return View(empresa);
-        //        }
-
-        //        // POST: PrincipalPage/Edit/5
-        //        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        //        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //        [HttpPost]
-        //        [ValidateAntiForgeryToken]
-        //        public async Task<IActionResult> Edit(int id, [Bind("empresaID,nombre,correo,telefono")] empresa empresa)
-        //        {
-        //            if (id != empresa.empresaID)
-        //            {
-        //                return NotFound();
-        //            }
-
-        //            if (ModelState.IsValid)
-        //            {
-        //                try
-        //                {
-        //                    _context.Update(empresa);
-        //                    await _context.SaveChangesAsync();
-        //                }
-        //                catch (DbUpdateConcurrencyException)
-        //                {
-        //                    if (!empresaExists(empresa.empresaID))
-        //                    {
-        //                        return NotFound();
-        //                    }
-        //                    else
-        //                    {
-        //                        throw;
-        //                    }
-        //                }
-        //                return RedirectToAction(nameof(Index));
-        //            }
-        //            return View(empresa);
-        //        }
-
-        //        // GET: PrincipalPage/Delete/5
-        //        public async Task<IActionResult> Delete(int? id)
-        //        {
-        //            if (id == null || _context.empresa == null)
-        //            {
-        //                return NotFound();
-        //            }
-
-        //            var empresa = await _context.empresa
-        //                .FirstOrDefaultAsync(m => m.empresaID == id);
-        //            if (empresa == null)
-        //            {
-        //                return NotFound();
-        //            }
-
-        //            return View(empresa);
-        //        }
-
-        //        // POST: PrincipalPage/Delete/5
-        //        [HttpPost, ActionName("Delete")]
-        //        [ValidateAntiForgeryToken]
-        //        public async Task<IActionResult> DeleteConfirmed(int id)
-        //        {
-        //            if (_context.empresa == null)
-        //            {
-        //                return Problem("Entity set 'trabajosDbContext.empresa'  is null.");
-        //            }
-        //            var empresa = await _context.empresa.FindAsync(id);
-        //            if (empresa != null)
-        //            {
-        //                _context.empresa.Remove(empresa);
-        //            }
-
-        //            await _context.SaveChangesAsync();
-        //            return RedirectToAction(nameof(Index));
-        //        }
-
-        //        private bool empresaExists(int id)
-        //        {
-        //          return (_context.empresa?.Any(e => e.empresaID == id)).GetValueOrDefault();
-        //        }
     }
 }
